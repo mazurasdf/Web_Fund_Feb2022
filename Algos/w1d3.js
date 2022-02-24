a564bee892f8b03c5a0980af98337cc3876217b3
mazurasdf <mazurasdf@gmail.com>
2022-02-24 10:15:22 -0600

    w1d3 algorithm

---
+++ Algos/w1d3.js
@@ -0,0 +1,38 @@
+//predict the output 1
+// var isSunny = true;
+// var temperature = 18;
+// var isSnowing = false;
+// var whatToBring = "I should bring: ";
+
+// if(isSunny){
+//     whatToBring += "sunglasses, ";
+// }
+// if(temperature < 50){
+//     whatToBring += "acoat, ";
+// }
+// if(isSnowing){
+//     whatToBring += "boots, ";
+// }
+// whatToBring += "and a smile!";
+
+// console.log(whatToBring);
+
+//predict the output 2
+// for(var i = 10; i > 0; i--){
+//     if(i !== 2){
+//         console.log(i);
+//     }
+//     else{
+//         console.log("ignition!");
+//     }
+// }
+
+// console.log("liftoff!");
+
+//algo time!!
+var countPositives = 0;
+var nums = [4,8,-15,-16,23,-42];
+
+//your code here!
+
+console.log(`there are ${countPositives} positive values`);
