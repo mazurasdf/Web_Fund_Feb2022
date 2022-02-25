98bf8bf650b4d803d62a33402cfda86eebcc4fc1
mazurasdf <mazurasdf@gmail.com>
2022-02-25 10:23:45 -0600

    w1d4 algo

---
+++ Algos/w1d4.js
@@ -0,0 +1,39 @@
+//predict the output 1
+// var x = 5;
+
+// function setX(value){
+//     x = value;
+// }
+
+// console.log(x);
+// setX(15);
+// console.log(x);
+
+//predict the output 2
+// var x = 5;
+
+// function addToX(amount) {
+//     return x + amount;
+//     console.log("hey it's me!");
+// }
+
+// console.log(x);
+// var result = addToX(-10);
+// console.log(result);
+// console.log(x);
+
+function isPal(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+        var right = arr.length-1-left;
+        if(arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
+}
+    
+var result1 = isPal( [1, 1, 2, 2, 1] );
+console.log(result1);
+    
+var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+console.log(result2);
